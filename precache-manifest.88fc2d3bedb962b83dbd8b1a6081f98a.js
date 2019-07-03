ed2ff06304f8d33eb51c27668464a20694ee3d78
Alex Breyfogle <alexbr@microsoft.com>
2019-07-03 07:19:15 -0700

    Updates

---
+++ precache-manifest.88fc2d3bedb962b83dbd8b1a6081f98a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5142bd467210d8a168ce06414f0112ea",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "b2e53269ecab11968dfa",
+    "url": "/shopping-list/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "bca2849149440897a7bc",
+    "url": "/shopping-list/static/js/2.b4ffa056.chunk.js"
+  },
+  {
+    "revision": "b2e53269ecab11968dfa",
+    "url": "/shopping-list/static/js/main.6a60c267.chunk.js"
+  },
+  {
+    "revision": "f83e989e4d16bb609454",
+    "url": "/shopping-list/static/js/runtime~main.e244f075.js"
+  }
+]);
